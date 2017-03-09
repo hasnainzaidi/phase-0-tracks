6b506ceb72ef569aeb75f681c0b9363db160f187
Hasnain Zaidi <s.hasnainzaidi@gmail.com>
2017-03-09 15:55:58 -0800

    add prompt for multiple employees

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,42 +1,67 @@
-puts "What is your name?"
-name = gets.chomp
+puts "How many employees will be processed?"
+enrollees = gets.chomp.to_i
 
-puts "How old are you? (years)"
-age = gets.chomp.to_i
+counter = 1
 
-puts "What year were you born?"
-birth_year = gets.chomp.to_i
+while counter <= enrollees
 
-calc_age = Time.now.year - birth_year
-age_valid = calc_age == age || calc_age == (age + 1)
+  puts "What is the name of candidate #{counter}?"
+  name = gets.chomp
 
-puts "Our company cafeteria serves garlic bread. Should we order some for you? (yes / no)"
-garlic_bread = gets.chomp.downcase
-case garlic_bread
-  when "y", "yes"
-    eats_garlic = true
-  when "n", "no"
-    eats_garlic = false
-end
+  puts "How old is candidate #{counter}? (years)"
+  age = gets.chomp.to_i
 
-puts "Would you like to enroll in the company’s health insurance?"
-insurance = gets.chomp
-case insurance
-  when "y", "yes"
-    wants_insurance = true
-  when "n", "no"
-    wants_insurance = false
-end
+  puts "What year was (s)he born?"
+  birth_year = gets.chomp.to_i
 
-##if age_valid && (eats_garlic || wants_insurance)
-##  puts "Probably not a vampire."
-##elsif !age_valid && !(eats_garlic || wants_insurance)
-##  puts "Almost certainly a vampire."
-##elsif !age_valid && !(eats_garlic && wants_insurance)
-##  puts "Probably a vampire."
-##end
+  calc_age = Time.now.year - birth_year
+  age_valid = calc_age == age || calc_age == (age + 1)
 
-case name
-when "Drake Cula", "Tu Fang"
-  puts "Definitely a vampire."
+  puts "Our company cafeteria serves garlic bread. Should we order some for him/her? (yes / no)"
+  garlic_bread = gets.chomp.downcase
+  case garlic_bread
+    when "y", "yes"
+      eats_garlic = true
+    when "n", "no"
+      eats_garlic = false
+  end
+
+  puts "Would candidate #{counter} like to enroll in the company’s health insurance?"
+  insurance = gets.chomp
+  case insurance
+    when "y", "yes"
+      wants_insurance = true
+    when "n", "no"
+      wants_insurance = false
+  end
+
+  vampire_status = "Results inconclusive."
+
+  if age_valid && (eats_garlic || wants_insurance)
+    vampire_status = "Probably not"
+  end
+
+  if !age_valid && !(eats_garlic || wants_insurance)
+    vampire_status = "Probably"
+  end
+
+  if !age_valid && !(eats_garlic && wants_insurance)
+    vampire_status = "Almost certainly"
+  end
+
+  case name
+  when "Drake Cula", "Tu Fang"
+    vampire_status = "Definitely"
+  end
+  
+  if vampire_status == "Results inconclusive."
+    puts "Status: #{vampire_status}"
+  else
+    puts "Status: #{vampire_status} a vampire."
+  end
+
+  puts "--"
+  puts ""
+
+  counter += 1
 end
