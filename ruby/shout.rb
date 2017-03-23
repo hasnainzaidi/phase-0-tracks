8ab27ff34d83097ce5f3c3ac8008105ee1c8e15b
Hasnain Zaidi <s.hasnainzaidi@gmail.com>
2017-03-23 13:17:43 -0700

    add happy and angry yelling

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,2 +1,14 @@
 module Shout
+
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def self.yell_happily(words)
+    words.upcase + "!!!" + " :D"
+  end
+
 end
+
+p Shout.yell_angrily("you suck")
+p Shout.yell_happily("but I love you anyway")
